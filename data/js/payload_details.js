0d5e94716177411d065f078d774323b77313db5f
seungow <seungow@gmail.com>
2017-05-02 02:22:54 +0900

    Initial

---
+++ data/js/payload_details.js
@@ -0,0 +1,5 @@
+var payload_details =  {
+  "tweets" : 126884,
+  "created_at" : "2017-04-30 15:10:45 +0000",
+  "lang" : "ko"
+}
---
+++ data/js/tweets/2017_05.js
@@ -0,0 +1,59 @@
+Grailbird.data.tweets_2017_05 = 
+[ {
+  "source" : "\u003Ca href=\"http:\/\/twitter.com\/download\/android\" rel=\"nofollow\"\u003ETwitter for Android\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ {
+      "expanded_url" : "https:\/\/twitter.com\/trustyune\/status\/858693865456320514\/photo\/1",
+      "indices" : [ 45, 68 ],
+      "url" : "https:\/\/t.co\/4oM4KqZyaR",
+      "media_url" : "http:\/\/pbs.twimg.com\/media\/C-qxgxRVYAA7kkb.jpg",
+      "id_str" : "858693855507472384",
+      "id" : 858693855507472384,
+      "media_url_https" : "https:\/\/pbs.twimg.com\/media\/C-qxgxRVYAA7kkb.jpg",
+      "sizes" : [ {
+        "h" : 428,
+        "resize" : "fit",
+        "w" : 640
+      }, {
+        "h" : 428,
+        "resize" : "fit",
+        "w" : 640
+      }, {
+        "h" : 428,
+        "resize" : "fit",
+        "w" : 640
+      }, {
+        "h" : 150,
+        "resize" : "crop",
+        "w" : 150
+      }, {
+        "h" : 428,
+        "resize" : "fit",
+        "w" : 640
+      } ],
+      "display_url" : "pic.twitter.com\/4oM4KqZyaR"
+    } ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "in_reply_to_status_id_str" : "858696772834570240",
+  "geo" : { },
+  "id_str" : "858697684802166784",
+  "in_reply_to_user_id" : 95255804,
+  "text" : "\uAC8C\uB2E4\uAC00 \uBB3858 \uC2EC0 \uC774\uBA74 \uB354 \uAC15\uD55C \uB3D9\uB825\uC774 \uB418\uACA0\uC9C0... \uC544\uB2C8 \uC774\uB7F0\uAC78 \uBB3C\uC5B4\uBD10\uC57C \uC544\uB098 https:\/\/t.co\/4oM4KqZyaR",
+  "id" : 858697684802166784,
+  "in_reply_to_status_id" : 858696772834570240,
+  "created_at" : "2017-04-30 15:00:56 +0000",
+  "in_reply_to_screen_name" : "crzbro",
+  "in_reply_to_user_id_str" : "95255804",
+  "user" : {
+    "name" : "\uBBF8\uC624",
+    "screen_name" : "crzbro",
+    "protected" : false,
+    "id_str" : "95255804",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/804406816092209152\/4m9Qk-sm_normal.jpg",
+    "id" : 95255804,
+    "verified" : false
+  }
+} ]
